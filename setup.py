3e6179e7329751c1d2c8c34414a7d61044b1a8c3
Naman-jain7 <nj323875@gmail.com>
2026-02-13 12:13:09 +0530

    update setup

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 setup(
-    name='Topsis-Naman-102316108',
-    version='0.0.1',
-    author='Naman',
-    author_email='njain_be23@thapar.edu',
-    description='TOPSIS implementation as a command line tool',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
+    name="topsis-naman-102316108",
+    version="0.0.2",
+    author="Naman",
+    author_email="njain_be23@thapar.edu",
+    description="TOPSIS implementation as a command line tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires='>=3.11',
-    install_requires=[
-        'pandas',
-        'numpy'
-    ],
-    entry_points={
-        'console_scripts': [
-            'topsis=Topsis-Naman-102316108.topsis:main'
-        ]
-    },
+    python_requires=">=3.11",
+    install_requires=["pandas", "numpy"],
+    entry_points={"console_scripts": ["topsis=Topsis_Naman_102316108.topsis:main"]},
     classifiers=[
-        'Programming Language :: Python :: 3', 
-        'Programming Language :: Python :: 3.11',
-        "Operating System :: OS Independent", 
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
     ],
 )
